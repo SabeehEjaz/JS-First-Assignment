fc142115255cfae807fcc2730a3a0e806a0624a5
sabeeh <sabeehejaz98@gmail.com>
2024-09-08 22:31:27 +0500

    JavaScript

---
+++ index/app.js
@@ -0,0 +1,186 @@
+// Variables
+
+// Bio  data using JS 
+// var firstname = "sabeeh"
+
+// var lastname = "Ejaz"
+
+// var fullname = "Sabeeh Ejaz"
+
+// var city = "Karachi"
+
+// var Education = "Undergraduate"
+
+// console.log( 
+//     "Hey my name is sabeeh ejaz. I live in karachi. I am an undergraduate right now ")
+
+//     console.log( 
+//         "Hey my name is " + fullname + ". I live in " + city + ". I am an " + Education + "right now")
+
+//  ---------------------------------------------------------------------------------------------------------------------------
+
+// Chapter 1 Alerts
+
+// Q1
+
+// alert("Welcome to my website")
+
+// Q2
+
+// alert ("Error! Please enter a valid password")
+
+// Q3
+
+// alert("Welcome to JS Land \nHappy Coding!")
+
+// Q4
+// alert("Welcome to JS Land")
+// alert("Prevent the page from creating additional dialogs")
+
+// Q5
+// alert("Hello.. I can run JS through my web browser console")
+
+// Chapter 1 Complete
+
+
+// Chapter 2 Strings Started
+
+// Q1
+// let a = "username"
+
+// Q2
+// let myName = "SabeehEjaz"
+// console.log(myName);
+
+// Q3
+// var message = "Hello World"
+// alert("Hello World")
+
+// Q4
+// let name = "sabeeh"
+// alert(name)
+
+
+// let age = "21"
+// alert(age)
+
+// let profession = "Certified Mobile App Development"
+// alert(profession)
+
+// Q5
+// alert("PIZZA \nPIZZ \nPIZ \nPI \nP")
+
+// Q6
+// var mail = "sabeehejaz98@gmail.com"
+// alert(mail)
+
+// Q7
+// var book = "A smarter way to learn Java Script"
+// alert(book)
+
+// Q9
+// var icon = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+// alert(icon)
+
+// Chapter 3 Numbers
+
+// Q1
+// let age = "I am " + 21 + "years old"
+// alert(age)
+
+// Q2
+// let visit = "You have visited thid site 14 times"
+
+// Q3
+// var birthyear = "My birth year is 2001 \n data type of my decleared variable is number "
+// document.write(birthyear)
+
+// Q4
+// var visitorName = "John Doe";        // Visitor's name
+// var productTitle = "T-shirt";        // Product title
+// var quantity = 5;
+
+//  // Step 3: Create the message to display
+//  var message = visitorName + " ordered " + quantity + " " + productTitle + "(s) on XYZ Clothing store";
+
+//  // Step 4: Display the message in the browser
+//  document.getElementById("orderMessage").textContent = message;
+
+// Chapter 4 legal and illegal
+
+// Q1
+// var visitorName = "John Doe", productTitle = "T-shirt", quantity = 5;
+
+// Q2
+// var firstName;
+// var _lastName;
+// var $age;
+// var user1;
+// var total_score;
+
+// var 1name;       // Starts with a number
+// var user-name;   // Contains a hyphen
+// var first name;  // Contains a space
+// var return;      // Uses a reserved word
+
+
+// Chapter 1 alerts short
+
+// Q1
+// alert("Sabeeh")
+// alert("SyedEjaz")
+// alert("sabeehejaz98@gmail.com")
+// alert("03122647772")
+// alert("Password")
+
+// Q2
+// alert("You're learning JavaScript!");
+
+// Q3
+// alert("Please pay for visiting this page")
+// .......................................................
+
+// Chapter 2 Variables
+
+// Q1
+
+// var firstName = "SabeehEjaz"
+
+// Q2
+// var myVariable;
+
+// myVariable = "Hello, world!";
+
+// Q3
+// var teamName = "TeamCT"
+// alert(teamName)
+
+// Q4
+// var bestMan = "Charlie";  
+// bestMan = "John";         
+// ......................................................AbortController.
+
+// Chapter 3 Variables numbers short
+// Q1
+// var caseQty;
+
+
+// // Q2
+// var caseQty = 144
+// caseQty = 144;
+
+// Q3
+// var num = "9";        // Initial value as a string
+// num = Number(num);    // Convert the string to a number
+
+// Q4
+// var total;
+// total = 5 + 2;
+
+// Q5
+// var merchTotal = 100;
+// var shipingCharge= 10;
+// var orderTotal = merchTotal + shipingCharge;
+// console.log(orderTotal);  // This will output 110
+
+
